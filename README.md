bffd4ce56e80209e6a2d246b46e90977db98cb93
Daniel <lanigela@gmail.com>
2017-08-18 12:34:30 -0400

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# ThreekitForWoocommerce
+=== ThreekitForWoocommerce ===
+Contributors: daniel@exocortex.com
+Requires WordPress: 4.0.0
+Tested WordPress: 4.8.1
+License: MIT License
+URI: https://opensource.org/licenses/MIT
+WC requires at least: 3.0
+WC tested up to: 3.1.2
---
+++ init.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: Threekit for WooCommerce
+ * Plugin URI: https://github.com/lanigela/ThreekitForWoocommerce
+ * Description: Your extension's description text.
+ * Version: 1.0.0
+ * Author: Daniel Zhou
+ * Author URI: daniel@exocortex.com
+ * Developer: Exocortex
+ * Developer URI: http://exocortex.com/
+ * Text Domain: Threekit-for-WooCommerce
+ *
+ * Copyright: © 2017 Exocortex
+ * License: MIT License
+ * License URI: https://opensource.org/licenses/MIT
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+/**
+ * Check if WooCommerce is active
+ **/
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+    // Put your plugin code here
+}
+
+?>
