fb1a46eaca6be6bb31b04cd5012cfae7655123bb
Daniel <lanigela@gmail.com>
2017-08-22 16:51:04 -0400

    debug

--- templates/single-product/clara-player.php
+++ templates/single-product/clara-player.php
@@ -13,5 +13,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 
-<div class="" id="clara-player" style="height:100px">
+<div class="<?php echo esc_attr( implode( ' ', array_map( 'sanitize_html_class', $wrapper_classes ) ) ); ?>" data-columns="<?php echo esc_attr( $columns ); ?>" style="opacity: 0; transition: opacity .25s ease-in-out;">
+  <div class="" id="clara-player" style="height:400px"></div>
 </div>
