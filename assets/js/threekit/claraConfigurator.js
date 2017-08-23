39bb39109ede5234a8a9504ae59796b471b60502
Daniel <lanigela@gmail.com>
2017-08-23 14:44:57 -0400

    get configurator form

--- assets/js/threekit/claraConfigurator.js
+++ assets/js/threekit/claraConfigurator.js
@@ -1,25 +1,62 @@
+"use strict";
+
 (function() {
-  "use strict";
-
-  var api = claraplayer('clara-player');
-  var claraSceneId = php_vars.clarauuid;
-  var configuratorEl = document.getElementById('panel-embed');
-  var configuratorForm = "Form1";
-
-  // Fetch and initialize the sceneId
-  api.sceneIO
-    .fetchAndUse(claraSceneId, null, { waitForPublish: true })
-    .then(() => {
-
-      if (configuratorForm && configuratorEl) {
-        api.configuration.initConfigurator({
-          id: claraSceneId,
-          form: configuratorForm,
-          el: configuratorEl
-        });
-
-        //api.on('configurationChange', onConfigurationChange({ api }));
-      }
-    });
+  var opts = {
+    playerDivId       : 'clara-player',
+    claraSceneId      : php_vars.clarauuid,
+    configuratorDivId : document.getElementById('panel-embed')
+  };
+  var cc = new claraConfigurator(opts);
 
 }());
+
+class claraConfigurator {
+  constructor(config) {
+    this.api                = null;
+    this.claraSceneId       = null;
+    this.playerDivId        = null;
+    this.configuratorDivId  = null;
+    this.configuratorForm   = null;
+
+    if (config) {
+      // init variables
+      this.playerDivId          = config.playerDivId;
+      this.claraSceneId         = config.claraSceneId;
+      this.configuratorDivId    = config.configuratorDivId;
+    }
+
+    this._initClara();
+  }
+
+  _initClara() {
+    var self = this;
+    // Fetch and initialize the sceneId
+    this.api = claraplayer(this.playerDivId);
+    this.api.sceneIO
+      .fetchAndUse(this.claraSceneId, null, { waitForPublish: true })
+      .then(() => {
+
+        self._getConfiguratorForm();
+
+        if (self.configuratorForm && self.configuratorEl) {
+          self.api.configuration.initConfigurator({
+            id    : self.claraSceneId,
+            form  : self.configuratorForm,
+            el    : self.configuratorEl
+          });
+
+          //api.on('configurationChange', onConfigurationChange({ api }));
+        }
+      });
+  }
+
+  _getConfiguratorForm() {
+    if (!this.api) {
+      return;
+    }
+    var forms = api.configuration.getForms();
+    if (forms.length > 0) {
+      this.configuratorForm = forms[0].name;
+    }
+  }
+}
